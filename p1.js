7fc1cef70f01c85ddd46f0890cafc9eb1bc4bd11
MehtaParth2322 <mehtaparth2322@gmail.com>
2020-08-20 13:47:24 +0530

    first commit

---
+++ p1.js
@@ -0,0 +1,78 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function myFunction() {
+        var x=document.getElementById("d1");
+        x.style.display="none";
+        var element = document.getElementById("d2");
+        element.classList.add("datashow");
+        }
+
+function myFunction1() {
+        var x=document.getElementById("d1");
+        x.style.display="none";
+        var element = document.getElementById("d3");
+        element.classList.add("datashow");
+        }
+
+function myFunction2() {
+        var x=document.getElementById("d1");
+        x.style.display="none";
+        var element = document.getElementById("d4");
+        element.classList.add("datashow");
+        }
+
+
+function myFunction3() {
+        var x=document.getElementById("d1");
+        x.style.display="none";
+        var element = document.getElementById("d5");
+        element.classList.add("datashow1");
+        }
+
+
+function myFunction4() {
+        var x=document.getElementById("d1");
+        x.style.display="none";
+        var element = document.getElementById("d6");
+        element.classList.add("datashow2");
+        }
+
+        
+function myFunction5() {
+        var x=document.getElementById("d1");
+        x.style.display="none";
+        var element = document.getElementById("d7");
+        element.classList.add("datashow2");
+        }
+
+function myFunction6() {
+        var x=document.getElementById("d1");
+        x.style.display="none";
+        var element = document.getElementById("d8");
+        element.classList.add("datashow2");
+        }
+
+function myFunction7() {
+        var x=document.getElementById("d1");
+        x.style.display="none";
+        var element = document.getElementById("d9");
+        element.classList.add("datashow2");
+        }
+
+function myFunction8() {
+        var x=document.getElementById("d1");
+        x.style.display="none";
+        var element = document.getElementById("d11");
+        element.classList.add("datashow2");
+        }
+
+
+
+
+
